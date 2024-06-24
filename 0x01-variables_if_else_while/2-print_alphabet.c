6e85622a916203919d42f0c20e3f7eff787f976e
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-06-24 14:49:37 +0100

    print the last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
