8d190c494ddef92b647b26f39d13c42f0ccd29a7
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-06-26 14:45:31 +0100

    add funtion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - function that carry out addition of two numbers
+* @a: first value
+* @b: second value
+* Return: returns the result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
