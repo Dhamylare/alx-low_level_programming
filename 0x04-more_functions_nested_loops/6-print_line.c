ec03b8ad85554b55849ebc07c33d8939695858dc
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-06-28 06:57:59 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_line - prints a straight line on the screen
+* @n: number of times the _ is printed
+* Return: reurns result
+*/
+
+void print_line(int n)
+{
+	int k;
+
+	for (k = 0; k <= n; k++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
