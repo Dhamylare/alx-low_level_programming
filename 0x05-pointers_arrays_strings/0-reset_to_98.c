94ddfe4e40c7bd805adf026e8f326e76fbf4d115
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-07-01 23:33:19 +0100

    rest the value of variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - resets the value of an array element in memory using pointer
+* Return: returns void
+* @n: parameter being manipulated
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Arraya, Pointers And Strings In C
+I am now learning about arrays and pointers and how to manipulate data using arrays and pointers.
