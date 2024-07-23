de4704fc2ed4983a55e31cad449f308aa51bda8a
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-07-23 20:22:42 +0100

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# 0x0E-structures and typedef
+This is the directory that explains the structures and typedef use case in C programming.
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+* struct dog - prints the details of a dog
+*
+* @name: name of the dog
+* @age: tells how old the dog is
+* @owner: tell the name of the owner of the dog
+* Return: no retur
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
