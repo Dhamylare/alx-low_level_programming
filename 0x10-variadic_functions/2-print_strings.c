b4ae590a1d9a87f572fb078fa962cf68c9e964c2
Oguntoye Ridwan <oguntoyer@yahoo.com>
2024-07-25 13:19:44 +0100

    function prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+* print_strings - prints all strings parsed
+* @separator: string printed between the strings.
+* @n: number of strings parsed
+* Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	i = 0;
+	while (i < n)
+	{
+		str = va_arg(ap, char*);
+		printf("%s", str);
+
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+
+		if (str == NULL)
+			printf("nil");
+
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
